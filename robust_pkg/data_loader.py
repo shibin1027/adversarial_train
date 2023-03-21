8252e3f7f2f0594aa232689ac682103265845d00
shibin1027 <shibin.mei1027@gmail.com>
2023-03-21 22:14:31 +0800

    add svhn train script

--- robust_pkg/data_loader.py
+++ robust_pkg/data_loader.py
@@ -17,6 +17,14 @@ def clean_loader_cifar100(args):
     clean_loader= DataLoader(dataset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
     return clean_loader
 
+def clean_loader_svhn(args):
+    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+    dataset = datasets.SVHN('/home/lorne/shibin/adversarial_train/data', split='test', transform=transforms.Compose([
+        transforms.ToTensor(),
+    ]))
+    clean_loader= DataLoader(dataset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
+    return clean_loader
+
 def adv_loader_data(args, adv_samples, targets):
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
     adv_loader = DataLoader(TensorDataset(adv_samples, targets), batch_size=args.test_batch_size, shuffle=False, **kwargs)
